5cda16039040a7ac56c64830dc7d277e8b00ed6d
loziuu <loz.iov@gmail.com>
2024-12-06 15:00:04 +0100

    Add day 6

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::fs::File;
+use std::{fs::File, time::Instant};
 
 mod five;
 mod four;
@@ -10,28 +10,52 @@ mod two;
 fn main() {
     println!("--- DAY 1 ---");
     println!("{}", one::first::execute("inputs/one"));
+    elapsed(|| one::first::execute("inputs/one"));
     println!("{}", one::second::execute("inputs/one"));
+    elapsed(|| one::second::execute("inputs/one"));
 
     println!("--- DAY 2 ---");
     println!("{}", two::first::execute(file("inputs/two")));
+    elapsed(|| two::first::execute(file("inputs/two")) as i32);
     println!("{}", two::second::execute(file("inputs/two")));
+    elapsed(|| two::second::execute(file("inputs/two")) as i32);
 
     println!("--- DAY 3 ---");
     println!("{}", three::first::execute(file("inputs/three")));
+    elapsed(|| three::first::execute(file("inputs/three")) as i32);
     println!("{}", three::second::execute(file("inputs/three")));
+    elapsed(|| three::second::execute(file("inputs/three")) as i32);
 
     println!("--- DAY 4 ---");
     println!("{}", four::first::execute(file("inputs/four")));
+    elapsed(|| four::first::execute(file("inputs/four")) as i32);
     println!("{}", four::second::execute(file("inputs/four")));
+    elapsed(|| four::second::execute(file("inputs/four")) as i32);
 
     println!("--- DAY 5 ---");
     println!("{}", five::first::execute("inputs/five"));
+    elapsed(|| five::first::execute("inputs/five"));
     println!("{}", five::second::execute("inputs/five"));
+    elapsed(|| five::second::execute("inputs/five"));
 
     println!("--- DAY 6 ---");
     println!("{}", six::first::execute("inputs/six"));
+    elapsed(|| three::first::execute(file("inputs/three")) as i32);
+    println!("{}", six::second::execute("inputs/six"));
+    elapsed(|| three::first::execute(file("inputs/three")) as i32);
 }
 
 fn file(name: &str) -> File {
     File::open(name).unwrap()
+}
+
+fn elapsed<T>(func: T)
+where
+    T: FnOnce() -> i32,
+{
+    let start = Instant::now();
+
+    func();
+
+    println!("Took: {:?}", start.elapsed());
 }
--- src/six/second.rs
+++ src/six/second.rs
@@ -15,29 +15,40 @@ pub fn execute(file: &str) -> i32 {
         })
         .collect();
 
-    mark_patroled(&mut matrix);
+    let m = matrix.len();
+    let n = matrix[0].len();
+    let start = mark_patroled(&mut matrix);
 
     let mut result = 0;
 
+    let mut visited = vec![vec![vec![false; DIR.len()]; n]; m];
     for i in 0..matrix.len() {
         for j in 0..matrix[0].len() {
             if matrix[i][j] == 'X' {
-                result += 1;
+                matrix[i][j] = '#';
+
+                if has_loop(start.0 as i32, start.1 as i32, 0, &matrix, &mut visited) == 1 {
+                    result += 1;
+                }
+
+                matrix[i][j] = 'X';
             }
         }
     }
+
     result
 }
 
-fn mark_patroled(matrix: &mut [Vec<char>]) {
+fn mark_patroled(matrix: &mut [Vec<char>]) -> (usize, usize) {
     for i in 0..matrix.len() {
         for j in 0..matrix[0].len() {
             if matrix[i][j] == '^' {
                 traverse(i as i32, j as i32, 0, matrix);
-                return;
+                return (i, j);
             }
         }
     }
+    (0, 0)
 }
 
 fn traverse(i: i32, j: i32, dir: usize, matrix: &mut [Vec<char>]) -> i32 {
@@ -50,6 +61,7 @@ fn traverse(i: i32, j: i32, dir: usize, matrix: &mut [Vec<char>]) -> i32 {
     }
 
     matrix[i as usize][j as usize] = 'X';
+
     let n = traverse(i + DIR[dir].0, j + DIR[dir].1, dir, matrix);
 
     if n == -1 {
@@ -58,6 +70,41 @@ fn traverse(i: i32, j: i32, dir: usize, matrix: &mut [Vec<char>]) -> i32 {
     }
 
     1 + n
+}
+
+fn has_loop(
+    i: i32,
+    j: i32,
+    dir: usize,
+    matrix: &[Vec<char>],
+    visited: &mut [Vec<Vec<bool>>],
+) -> i32 {
+    if i < 0 || j < 0 || i >= matrix.len() as i32 || j >= matrix[0].len() as i32 {
+        return 0;
+    }
+
+    if matrix[i as usize][j as usize] == '#' {
+        return -1;
+    }
+
+    if visited[i as usize][j as usize][dir] {
+        return 1;
+    }
+    visited[i as usize][j as usize][dir] = true;
+
+    let mut n = -1;
+    let mut dir = dir;
+    let temp = dir;
+
+    while n == -1 {
+        // This can return -1
+        n = has_loop(i + DIR[dir].0, j + DIR[dir].1, dir, matrix, visited);
+
+        dir = next(dir);
+    }
+
+    visited[i as usize][j as usize][temp] = false;
+    n
 }
 
 fn next(dir: usize) -> usize {
@@ -69,9 +116,9 @@ mod tests {
     use super::execute;
 
     #[test]
-    fn first() {
+    fn second() {
         let result = execute("inputs/six_test");
 
-        assert_eq!(41, result);
+        assert_eq!(6, result);
     }
 }
